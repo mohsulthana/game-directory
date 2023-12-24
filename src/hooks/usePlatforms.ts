faaea06b962e91708acc011c3431a3c2b58c3b32
Mohammad Sulthan <muhammadsultana@outlook.com>
2023-12-24 19:20:31 +0700

    refactor entities

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import platforms from "../data/platforms";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import APIClient, { FetchResponse } from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
